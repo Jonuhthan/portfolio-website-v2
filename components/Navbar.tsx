c04994f31fbcd01c2370bb3a1c49a311e136fc6a
Jonathan Lam <jonhaolam@gmail.com>
2024-10-15 14:44:48 -0400

    Added menu button functionality with navigation items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll/modules";
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
@@ -24,10 +23,10 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     page: "projects",
   },
-]
+];
 
 const Navbar = () => {
-  const {systemTheme, theme, setTheme } = useTheme();
+  const { systemTheme, theme, setTheme } = useTheme();
   // tracker for current theme
   const currentTheme = theme === "system" ? systemTheme : theme;
   // keeps track of mobile/desktop view for menu button
@@ -37,58 +36,75 @@ const Navbar = () => {
     // different styling according to current theme
     <header
       className={`w-full mx-auto px-4 shadow fixed top-0 z-50 sm:px-20 ${
-        currentTheme === 'dark' ? 'bg-stone-900 border-b border-stone-600' : 'bg-white'
+        currentTheme === "dark" ? "bg-stone-900 border-b border-stone-600" : "bg-white"
       }`}
     >
-      <div className='justify-between md:items-center md:flex'>
+      <div className="justify-between md:items-center md:flex">
         <div>
-          <div className='flex items-center justify-between py-3'>
+          <div className="flex items-center justify-between py-3">
             {/* logo */}
-            <div className='md:py-5 md:block'>
-              <h2 className='text-2xl font-bold'>Jonathan Lam</h2>
+            <div className="md:py-5 md:block">
+              <h2 className="text-2xl font-bold">Jonathan Lam</h2>
             </div>
-            <div className='md:hidden'>
-              {/* menu button */}
-              <button >
+            <div className="md:hidden">
+              {/* menu button, shows or closes nav items */}
+              <button onClick={() => setNavbar(!navbar)}>
                 {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
               </button>
             </div>
           </div>
         </div>
-          <div>
-            {/* hides or shows menu button according to viewport */}
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? "block" : "hidden"
-              }`}
-            >
-              {/* displays all nav items and light/dark mode toggle button */}
-              <div className='items-center justify-center md:flex md:space-x-6'>
-                {NAV_ITEMS.map((item, idx) => {
-                  return <a key={idx}>{item.label}</a>;
-                })}
-                {currentTheme === 'dark' ? (
-                  <button
-                    onClick={() => setTheme('light')}
-                    className='bg-slate-100 p-2 rounded-xl'
-                  >
-                    <RiSunLine size={25} color="black" />
-                  </button>
-                  ) : (
-                  <button
-                    onClick={() => setTheme('dark')}
-                    className='bg-slate-100 p-2 rounded-xl'
-                  >
-                    <RiMoonFill size={25} />
-                  </button>
-                )}
+        <div>
+          {/* hides or shows menu button according to viewport */}
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            {/* displays all nav items and light/dark mode toggle button */}
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => (
+                <Link
+                  key={idx}
+                  to={item.page}
+                  className={`block lg:inline-block cursor-pointer ${
+                    // update link text according to theme
+                    currentTheme === "dark"
+                      ? "text-neutral-100 hover:text-neutral-400"
+                      : "text-neutral-900 hover:text-neutral-500"
+                  }`}
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {item.label}
+                </Link>
+              ))}
+              {/* dark/light mode toggle button */}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
             </div>
           </div>
         </div>
       </div>
     </header>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
