e00ce292c35f21bc75ddffee08a9e12bc1048ac1
Jonathan Lam <jonhaolam@gmail.com>
2024-10-15 15:49:38 -0400

    Created about section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <Hero />
+      <About />
     </main>
   );
 }
