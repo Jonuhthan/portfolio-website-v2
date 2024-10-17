59c01aeb75d0a9d60448009f4388c16ecb2b07b7
Jonathan Lam <jonhaolam@gmail.com>
2024-10-16 22:27:36 -0400

    Spacing and down arrow adjustments

--- components/About.tsx
+++ components/About.tsx
@@ -17,8 +17,11 @@ const skills = [
 const About = () => {
   return (
     <section id="about">
-      <div>
-        <h1 className='text-center font-bold text-4xl'>About Me</h1>
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className='text-center font-bold text-4xl'>
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
+        </h1>
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
           <div className='md:w-1/2'>
             <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,13 +8,13 @@ import { HiArrowDown } from "react-icons/hi"
 const Hero = () => {
   return (
     <section id='home'>
-        <div className='flex flex-col text-center items-center justify-center my-10 py-16 sm:py-32 md:flex-row md:space-x-20 md:text-left md:py-52 '>
-            <div>
-                <Image className='rounded-3xl shadow-2xl' src="/profile.jpg" alt="missing" width={300} height={300}/>
+        <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+            <div className="md:mt-2 md:w-1/2">
+                <Image className='rounded-3xl shadow-2xl' src="/profile.jpg" alt="missing" width={325} height={325}/>
             </div>
-            <div className='md:mt-2 md:w-3/5'>
-                <h1 className='font-bold text-4xl mt-6 md:text-7xl md:mt-0'>Hi, I'm Jonathan!</h1>
-                <p className='text-lg mt-8 mb-8 md:text-2xl'>
+            <div className='md:mt-2 md:w-1/2'>
+                <h1 className='font-bold text-5xl mt-6 md:text-7xl md:mt-0'>Hi, I'm Jonathan!</h1>
+                <p className="text-lg mt-4 mb-6 md:text-2xl">
                     I'm a second-year {" "}
                     <span className='font-semibold text-teal-600'>
                     Computer Science student {" "}
@@ -45,7 +45,7 @@ const Hero = () => {
                 offset={-100}
                 duration={500}
             >
-                <HiArrowDown size={35} className="animate-bounce" />
+                <HiArrowDown size={45} className="animate-bounce" />
             </Link>
         </div>
     </section>
