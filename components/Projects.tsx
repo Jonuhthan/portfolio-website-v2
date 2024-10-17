c2d4963c3c4fcba387b5f1b9ac370daa928a2ceb
Jonathan Lam <jonhaolam@gmail.com>
2024-10-16 22:59:38 -0400

    Added SlideUp component to Projects component and modified tailwind config

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import SlideUp from './SlideUp';
 
 const projectsList = [
     {
@@ -19,8 +20,8 @@ const projectsList = [
         link: "",
     },    
     {
-        name: "Project 1",
-        description: "Project 1 description",
+        name: "Project 3",
+        description: "Project 3 description",
         image: "",
         github: "",
         link: "",
@@ -39,40 +40,43 @@ const Projects = () => {
             {/* populate with each project listed */}
             {projectsList.map((project, idx) => {
                 return (
-                <div key={idx}>
-                    <div className='flex flex-col md:flex-row md:space-x-12'>
-                        {/* project image */}
-                        <div className='md:w-1/2 mt-8'>
-                            <Link href={project.link} target='_blank'>
-                                <Image
-                                    src={project.image}
-                                    alt=""
-                                    width={1000}
-                                    height={1000}
-                                    className="rounded-xl shadow-xl hover:opacity-70"
-                                />
-                            </Link>
 
-                        </div>
-                        {/* text section for each project */}
-                        <div className='md:w-1/2 mt-12'>
-                            <h1 className='text-4xl font-bold mb-6'>
-                                {project.name}
-                            </h1>
-                            <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                                {project.description}
-                            </p>
-                            {/* github and project links */}
-                            <div className='flex flex-row align-bottom space-x-4'>
-                                <Link href={project.github} target='_blank'>
-                                    <BsGithub size={30} className='hover:translate-y-1 transition transform cursor-pointer' />
-                                </Link>
+                <div key={idx}>
+                    <SlideUp offset='-300px 0px -300px 0px'>
+                        <div className='flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
+                            {/* project image */}
+                            <div className='md:w-1/2 mt-8'>
                                 <Link href={project.link} target='_blank'>
-                                    <BsArrowUpRightSquare size={30} className='hover:translate-y-1 transition transform cursor-pointer' />
+                                    <Image
+                                        src={project.image}
+                                        alt=""
+                                        width={1000}
+                                        height={1000}
+                                        className="rounded-xl shadow-xl hover:opacity-70"
+                                    />
                                 </Link>
+
+                            </div>
+                            {/* text section for each project */}
+                            <div className='md:w-1/2 mt-12'>
+                                <h1 className='text-4xl font-bold mb-6'>
+                                    {project.name}
+                                </h1>
+                                <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                                    {project.description}
+                                </p>
+                                {/* github and project links */}
+                                <div className='flex flex-row align-bottom space-x-4'>
+                                    <Link href={project.github} target='_blank'>
+                                        <BsGithub size={30} className='hover:translate-y-1 transition transform cursor-pointer' />
+                                    </Link>
+                                    <Link href={project.link} target='_blank'>
+                                        <BsArrowUpRightSquare size={30} className='hover:translate-y-1 transition transform cursor-pointer' />
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </SlideUp>
                 </div>
                 )
             })};
